b63939151da76b58ddd71d64cc884ea4e2410c5d
Smooth Operator <bsaikou1990@gmail.com>
2023-08-20 01:25:05 -0400

    Added starter code forprompts

--- index.js
+++ index.js
@@ -9,17 +9,50 @@ const employeeDb = mysql.createConnection({
     password: '',
     // Inserts employees database from schema.sql
     database: 'employees_db'
+}, console.log('Welcome to the Employee database')
+);
+employeeDb.connect((error) => {
+    if (error) throw error;
+
+    primaryPrompt();
 });
 
-const questions = [{
+const primaryPrompt = () => [{
     type: 'list',
-    name: 'options',
-    message: 'Please choose from the following options:',
-    choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role'],
-}, {
-    
-}
-
-]
-
-inquirer.prompt(questions).then()
+    name: 'begin',
+    message: 'Please choose from the following:',
+    choices: ['View all departments',
+        'View all roles',
+        'View all employees',
+        'Add a department',
+        'Add a role',
+        'Add an employee',
+        'Update an employee role',
+        'Exit'],
+}]
+inquirer.prompt(primaryPrompt()).then((choice) => {
+    switch (choice.begin) {
+        case 'View all departments':
+            departments();
+            break;
+        case 'View all roles':
+            roles();
+            break;
+        case 'Add a department':
+            addDepartment();
+            break;
+        case 'Add a role':
+            addRole();
+            break;
+        case 'Add an Employee':
+            addEmployee();
+            break;
+        case 'Update an Employee role':
+            updateRole();
+            break;
+        case 'Exit':
+            exit();
+            break;
+        default: break;
+    }
+})
